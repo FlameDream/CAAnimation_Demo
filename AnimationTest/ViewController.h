9b97b61b685c20b52734cc2d663cc7bf5af41b6a
wangdongshen <dongshen.wang@ftimage.cn>
2018-09-17 10:04:25 +0800

    Initial Commit

---
+++ AnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTest
+//
+//  Created by ftimage2 on 2018/9/17.
+//  Copyright © 2018年 Jack Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnimationTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnimationTest
+//
+//  Created by ftimage2 on 2018/9/17.
+//  Copyright © 2018年 Jack Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
